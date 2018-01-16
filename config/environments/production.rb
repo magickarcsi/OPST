30a4efc7f042c903608bf20124b076c75ce51d7b
Marton Koran <koran.marci@gmail.com>
2018-01-16 22:54:10 +0000

    changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,8 +65,7 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
-  # Send deprecation notices to registered listeners.
-  config.active_support.deprecation = :notify
+  
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
@@ -81,6 +80,5 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+ 
 end
